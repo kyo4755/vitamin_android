18be9b1bff1576f401be1ce46fe2f747b4ba2b0b
jeonghun <anemoking@naver.com>
2018-04-15 16:45:19 +0900

    study2 인텐트완성

---
+++ app/src/main/java/com/vitamin/wecantalk/StudyActivity.java
@@ -0,0 +1,17 @@
+package com.vitamin.wecantalk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by anemo on 2018-04-15.
+ */
+
+public class StudyActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study2);
+    }
+
+}
--- app/src/main/java/com/vitamin/wecantalk/fragment/StudyFragment.java
+++ app/src/main/java/com/vitamin/wecantalk/fragment/StudyFragment.java
@@ -1,19 +1,26 @@
 package com.vitamin.wecantalk.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.vitamin.wecantalk.R;
+import com.vitamin.wecantalk.StudyActivity;
 
 /**
  * Created by JongHwa on 2018-04-13.
  */
 
 public class StudyFragment extends Fragment {
+
+    TextView tv;
+    Button RB;
+    Button LB;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,19 @@ public class StudyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
         View view = inflater.inflate(R.layout.activity_study, null);
+
+        tv=(TextView)view.findViewById(R.id.word_meaning);
+        tv.setText("돼지[명사]");
+
+        RB=(Button)view.findViewById(R.id.RButton);
+        LB=(Button)view.findViewById(R.id.LButton);
+
+        RB.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view) {
+                Intent it = new Intent(getActivity(), StudyActivity.class);
+                startActivity(it);
+            }
+        });
 
         return view;
     }
