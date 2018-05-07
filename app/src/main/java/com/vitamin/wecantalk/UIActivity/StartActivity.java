e4ce56e56bda970170f6024178fe1663dd872cc0
Kim Jong-Hwa <kyo4755@naver.com>
2018-05-07 15:40:36 +0900

    startactivity 약간 수정

--- app/src/main/java/com/vitamin/wecantalk/UIActivity/StartActivity.java
+++ app/src/main/java/com/vitamin/wecantalk/UIActivity/StartActivity.java
@@ -70,13 +70,10 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.start_input_login_button:
-                //tryLogin();
-                Intent it = new Intent(StartActivity.this, MainFragmentActivity.class);
-                startActivity(it);
-                finish();
+                tryLogin();
                 break;
             case R.id.start_input_register:
-                it = new Intent(StartActivity.this, RegisterActivity.class);
+                Intent it = new Intent(StartActivity.this, RegisterActivity.class);
                 startActivity(it);
                 finish();
                 break;
