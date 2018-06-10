f74b23e29314b1aa6c8c82a67f43b7428860fca9
jeonghun <anemoking@naver.com>
2018-06-10 17:11:09 +0900

    Merge branch 'master' of https://www.github.com/kyo4755/vitamin_android
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    
    브로드캐스트리시버 채팅수정중

--- app/src/main/java/com/vitamin/wecantalk/Adapter/CommunityRoomListViewAdapter.java
+++ app/src/main/java/com/vitamin/wecantalk/Adapter/CommunityRoomListViewAdapter.java
@@ -25,7 +25,7 @@ import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 public class CommunityRoomListViewAdapter extends BaseAdapter{
 
-    private static ArrayList<CommunityRoomListViewPOJO> list;
+    private ArrayList<CommunityRoomListViewPOJO> list;
 
     public CommunityRoomListViewAdapter(ArrayList<CommunityRoomListViewPOJO> list){
         this.list = list;
@@ -92,7 +92,7 @@ public class CommunityRoomListViewAdapter extends BaseAdapter{
         notifyDataSetChanged();
     }
 
-    public static void firebaseAddList(CommunityRoomListViewPOJO pojo){
+    public void firebaseAddList(CommunityRoomListViewPOJO pojo){
         list.add(pojo);
     }
 }
